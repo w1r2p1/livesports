674db1a6101e262e0fb290ab5bb38580b2f1c5dc
mati4x <mateusz.s@live.com>
2017-04-18 18:57:22 +0100

    switch lista sportow

--- livesports/Program.cs
+++ livesports/Program.cs
@@ -7,9 +7,34 @@ namespace livesports
         static void Main(string[] args)
         {
             string url = "http://flashscore.pl/";
-            Console.Write("Podaj nazwe dyscypliny: ");
-            string sportsname = Console.ReadLine();
 
+
+            Console.WriteLine("1 - Pilka nozna\n2 - Tenis\n3 - Hokej\n4 - Koszykówka\n5 - Siatkówka\n6 - Baseball");
+            Console.Write("Podaj numer lub nazwe dyscypliny: ");
+            string sportsname = Console.ReadLine();
+            switch (sportsname)
+            {
+                case "1":
+                    sportsname = "Piłka nożna";
+                    break;
+                case "2":
+                    sportsname = "Tenis";
+                    break;
+                case "3":
+                    sportsname = "Hokej";
+                    break;
+                case "4":
+                    sportsname = "Koszykówka";
+                    break;
+                case "5":
+                    sportsname = "Siatkówka";
+                    break;
+                case "6":
+                    sportsname = "Baseball";
+                    break;
+                default:
+                    break;
+            }
 
             Console.Write("Podaj nazwe druzyny: ");
             string teamname = Console.ReadLine();
